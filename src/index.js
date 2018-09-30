1359eeb2fe2752512e7bf13c6a4aad7c7a82cb2c
MDR <mrdemonrush@gmail.com>
2018-09-30 19:52:44 +0300

    zeros v1.0.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  
+  let i = 1;
+  let result = 0;
+
+  while( Math.trunc( number / Math.pow( 5, i ) ) ){
+
+      result += Math.trunc( number / Math.pow( 5, i++ ) );
+  }
+
+  return result;
 }
+
